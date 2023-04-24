55a172ea87a96445a411bf9fcd5bd3626015b694
Srini Iyer <srini.iyer9@gmail.com>
2023-04-24 14:29:19 -0400

    E2338 - add comments to test cases

--- test/models/response_map_test.rb
+++ test/models/response_map_test.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
+# references to sut refer to the "system under test". This is the target class for the unit test
 
+# Mock implementation of MetareviewResponseMap which can be used in unit tests that have a dependency on this class
 class MockMetareviewResponseMap
   attr_accessor :id
 end
 class ResponseMapTest < ActiveSupport::TestCase
 
+  # verified that instance method returns where a given partipant is the one who performed the meta review
   test "metareviewed_by?" do
     sut = ResponseMap.new
     sut.id = 1
@@ -17,6 +20,7 @@ class ResponseMapTest < ActiveSupport::TestCase
     end
   end
 
+  # verified that the instance method returns the correct metareview responsemap when a meta reviewer is assigned
   test "assign_metareviewer" do
     sut = ResponseMap.new
     sut.id = 1
@@ -45,11 +49,13 @@ class ResponseMapTest < ActiveSupport::TestCase
     end
   end
 
+  # verifies that the instance method returns whether a survey is applicable
   test "survey?" do
     sut = ResponseMap.new
     assert_equal false, sut.survey?
   end
 
+  # verfied that the instance method returns the correct assignment team for the response map
   test "reviewee_team" do
     sut = ResponseMap.new
     assignmentTeam = MockAssignmentTeam.new
